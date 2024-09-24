05d69b1f75571ee46259c804fece439428e375dd
Fathur <fathur260605@gmail.com>
2024-09-24 19:52:53 +0700

    test push tugas 4

--- main/views.py
+++ main/views.py
@@ -3,15 +3,25 @@ from main.forms import ItemEntryForm
 from main.models import ItemEntry
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     item_entries = ItemEntry.objects.all()
     context = {
         'title': 'Toko-ya',
-        'name': 'Fathurrahman Kesuma Ridwan',
+        'name': request.user.username,
         'class': 'PBP E',
-        'item_entries' : item_entries
+        'item_entries' : item_entries,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -20,7 +30,9 @@ def create_item_entry(request):
     form = ItemEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        item_entry = form.save(commit=False)
+        item_entry.user = request.user
+        item_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
@@ -42,7 +54,36 @@ def show_json_by_id(request, id):
     data = ItemEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def register(request):
+    form = UserCreationForm()
 
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
 
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
 
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
 
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
--- toko_ya/settings.py
+++ toko_ya/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ea7t7_7v5-z-at#(ff(wlcw8*d@djbx1c5^j*dlxw2*@c3lgio'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","http://fathurrahman-kesuma-tokoya2.pbp.cs.ui.ac.id", "fathurrahman-kesuma-tokoya2.pbp.cs.ui.ac.id"]
 
